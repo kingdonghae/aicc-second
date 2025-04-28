754ae9bbe1ba7f67018c3d8804907717d64e3477
5oran9 <sorang.lab@gmail.com>
2025-04-28 17:47:47 +0900

    feat. Login 페이지 레이아웃 제작

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import Home from "./pages/home"
 import Rank from "./pages/Rank"
+import Login from './pages/Login'
 
 function App() {
 
   return (
 
     <div>
-      <Rank></Rank>
+      {/* <Home></Home> */}
+      {/* <Rank></Rank> */}
+      <Login></Login>
     </div>
-    
+
   )
 }
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Login.css';
+
+const Login = () => {
+  return (
+    <div id='background'>
+        <div id='house'>
+            <div id='login-box'>
+                <div id='login'>
+                    <h2>로그인</h2>
+                    <input type="text" placeholder='아이디를 입력하세요.'/>
+                    <input type="password" placeholder='비밀번호를 입력하세요.' id='login-password'/>
+                    <span></span>
+                    <button>로그인</button>
+                    <button>회원가입</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
