d75d0e8555f5e95121aba4df325f71752c4deac4
5oran9 <sorang.lab@gmail.com>
2025-05-02 16:08:23 +0900

    feat: InfoDetail 그래프 페이지 레이아웃, APIMap 등 수정

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Searchbox = ({ inputValue, setInputValue, onSearch }) => {
-
   const navigate = useNavigate();
 
-
   const submitSearch = () => {
-    if(inputValue.trim()){
-      setInputValue(inputValue.trim())
-      onSearch(inputValue.trim())
-      navigate(`/map?address=${encodeURIComponent(inputValue.trim())}`);
+    if (inputValue.trim()) {
+      // ✅ alertShown 초기화
+      window.alertShown = false;
+
+      setInputValue(inputValue.trim());
+      onSearch(inputValue.trim());
+      navigate(`/map?address=${encodeURIComponent(inputValue.trim())}&key=${Date.now()}`);
     }
-  }
+  };
 
   const handleKeyDown = (e) => {
-    if(e.key === "Enter"){
+    if (e.key === "Enter") {
       e.preventDefault();
       submitSearch();
     }
@@ -26,21 +27,20 @@ const Searchbox = ({ inputValue, setInputValue, onSearch }) => {
     submitSearch();
   };
 
-
   return (
     <div className='search-page'>
-        <form id='search-input-box' onSubmit={handleClick}>
-          <input type="text"
+      <form id='search-input-box' onSubmit={handleClick}>
+        <input
+          type="text"
           placeholder='주소를 입력해주세요.'
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyDown={handleKeyDown}
-          />
-          <button id='input-button'></button>
-        </form>
-      
+        />
+        <button id='input-button'></button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbox
+export default Searchbox;
