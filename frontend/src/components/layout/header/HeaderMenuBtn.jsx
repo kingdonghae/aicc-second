4a4f34fbb1b5500779a65580fc7481eaee46c2a0
happyKJE <jungeunkim2007@gmail.com>
2025-05-07 16:14:03 +0900

    1. 수정:헤더 기능 누락된거 추가

--- frontend/src/components/layout/header/HeaderMenuBtn.jsx
+++ frontend/src/components/layout/header/HeaderMenuBtn.jsx
@@ -9,32 +9,23 @@ const HeaderMenuBtn = ({ menuRef }) => {
     const { goMap, goRank, goBoard } = useNavigation();
     const location = useLocation();
 
-    const isActive = (path) => location.pathname === path;
+    const isActive = (path) => location.pathname === `${path}`;
 
     return (
         <nav className="menu-popup" ref={menuRef}>
             <ul className="menu-group">
-                <li className="menu-list">
-                    <button
-                        onClick={goMap}
-                        className={isActive(ROUTES.MAP) ? 'active' : ''}
-                    >
+                <li className="menu-list" id={isActive(ROUTES.MAP) ? 'menu-select' : undefined} onClick={goMap}>
+                    <button>
                         <MapIcon style={{ fontSize: '2.5rem' }} />지도 보기
                     </button>
                 </li>
-                <li className="menu-list">
-                    <button
-                        onClick={goRank}
-                        className={isActive(ROUTES.RANK) ? 'active' : ''}
-                    >
+                <li className="menu-list" id={isActive(ROUTES.RANK) ? 'menu-select' : undefined} onClick={goRank}>
+                    <button>
                         <TrendingUpIcon style={{ fontSize: '2.5rem' }} />검색 순위
                     </button>
                 </li>
-                <li className="menu-list">
-                    <button
-                        onClick={goBoard}
-                        className={isActive(ROUTES.BOARD) ? 'active' : ''}
-                    >
+                <li className="menu-list" id={isActive(ROUTES.BOARD) ? 'menu-select' : undefined} onClick={goBoard}>
+                    <button>
                         <Diversity3Icon style={{ fontSize: '2.5rem' }} />정보 마당
                     </button>
                 </li>
